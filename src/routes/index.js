4fa63269b6c6ba807ee2536553c2598fe9332206
Gabriel <biel14022002@gmail.com>
2024-11-21 19:49:40 -0300

    fix: start script

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,9 @@ const verifyCpfExistController = require("../controllers/verifyCpfExist");
 const verifyUserExistController = require("../controllers/verifyUserExist");
 
 router.get("/", (req, res) => {
-  res.status(200).send({ message: "Hello World", seuIp: req.ip });
+  res
+    .status(200)
+    .send({ message: "Hello World", seuIp: req.ip, status: "LOW" });
 });
 router.post("/createuser", createUser);
 router.post("/verifyuser", verifyUserExistController);
