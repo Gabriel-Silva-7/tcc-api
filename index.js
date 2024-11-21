19fbe5f037c06273d8b8adf604a7742d0a0056f5
Gabriel <biel14022002@gmail.com>
2024-11-21 11:43:08 -0300

    fix: start script

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
